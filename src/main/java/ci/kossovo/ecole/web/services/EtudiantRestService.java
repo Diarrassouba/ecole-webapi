1946d3415ef941ad1aa8a11ba7fa4022788b75e4
Diarrassouba <ddrissaci@gmail.com>
2017-02-19 19:23:47 +0000

    Creation methode creer de l'etudiant

--- src/main/java/ci/kossovo/ecole/web/services/EtudiantRestService.java
+++ src/main/java/ci/kossovo/ecole/web/services/EtudiantRestService.java
@@ -1,15 +1,27 @@
 package ci.kossovo.ecole.web.services;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import ci.kossovo.ecole.entity.Adresse;
+import ci.kossovo.ecole.entity.Etudiant;
 import ci.kossovo.ecole.entity.Personne;
 import ci.kossovo.ecole.exceptions.InvalidPersonneException;
+import ci.kossovo.ecole.web.models.Reponse;
 import ci.kossovo.ecole.web.models.personne.ApplicationModelPersonne;
+import ci.kossovo.ecole.web.models.personne.PostAjoutEtudiant;
+import ci.kossovo.ecole.web.utilitaires.Static;
 
 @RestController
 public class EtudiantRestService {
@@ -19,23 +31,83 @@ public class EtudiantRestService {
 	@Autowired
 	private ObjectMapper jsonMapper;
 
-	
-	public Personne creer(Personne entity) throws InvalidPersonneException {
-		return modelPersonne.creer(entity);
+	// Methode local
+	private Reponse<Etudiant> getEtudiant(Long id) {
+		// On recupère létudiant
+		Etudiant etud = null;
+		try {
+			etud = (Etudiant) modelPersonne.find(id);
+		} catch (RuntimeException e) {
+			return new Reponse<>(1, Static.getErreurforexception(e), null);
+		}
+
+		// etudiant existe?
+		if (etud == null) {
+			List<String> messages = new ArrayList<>();
+			messages.add(String.format("L'étudiant [%s] n'existe pas", id));
+			return new Reponse<Etudiant>(2, messages, null);
+		}
+		// OK
+		return new Reponse<Etudiant>(0, null, etud);
 	}
 
+	////////////////////////////////////////////////////////////////////////////////////////////
+
+	// crée un etudiant
+	@PostMapping("/etudiants")
+	public String creer(@RequestBody PostAjoutEtudiant pe) throws JsonProcessingException {
+		Reponse<Etudiant> reponse = null;
+		boolean erreur = false;
+
+		// Affecter les champs de l'entité etudiant
+		Adresse ad = new Adresse(pe.getQuartier(), pe.getCodePostal(), pe.getEmail());
+		Etudiant entity = new Etudiant(pe.getTitre(), pe.getNom(), pe.getPrenom(), pe.getNumCni(), pe.getMatricule());
+
+		// formater la date qui est en String => html
+		String nais = pe.getDateNaissance();
+		Date naissance = null;
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setLenient(false);
+
+		try {
+			naissance = sdf.parse(nais);
+		} catch (ParseException e1) {
+			List<String> messages = new ArrayList<String>();
+			messages.add(String.format("La date [%s] est invalide", nais));
+			reponse = new Reponse<>(8, messages, null);
+			erreur = true;
+		}
+		// ajouter l'etudiant
+		if (!erreur) {
+			entity.setDateNaissance(naissance);
+
+			try {
+				reponse = new Reponse<Etudiant>(0, null, (Etudiant) modelPersonne.creer(entity));
+			} catch (InvalidPersonneException e) {
+				reponse = new Reponse<Etudiant>(1, Static.getErreurforexception(e), null);
+			}
+
+		}
+
+		return jsonMapper.writeValueAsString(reponse);
+	}
+
+	// modifier un etudiant
 	public Personne modifier(Personne entity) throws InvalidPersonneException {
 		return modelPersonne.modifier(entity);
 	}
 
+	// chercher un etudiant par identifiant
 	public Personne find(Long id) {
 		return modelPersonne.find(id);
 	}
 
+	// supprimer des etudiant
 	public void spprimer(List<Personne> entities) {
 		modelPersonne.spprimer(entities);
 	}
 
+	// supprimer un etudiant par identifiant
 	public boolean supprimer(Long id) {
 		return modelPersonne.supprimer(id);
 	}
